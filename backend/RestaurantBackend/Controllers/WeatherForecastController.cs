7f0aa35dd126d963c9accaa24d8b380604ded52c
Trivex Tech <ashan.trivextech@gmail.com>
2026-01-26 21:57:29 +0530

    mid

--- backend/RestaurantBackend/Controllers/WeatherForecastController.cs
+++ backend/RestaurantBackend/Controllers/WeatherForecastController.cs
@@ -22,4 +22,17 @@ public class WeatherForecastController : ControllerBase
         })
         .ToArray();
     }
+
+
+    [HttpGet("/aaa")]
+    public IEnumerable<WeatherForecast> GetAA()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
 }
