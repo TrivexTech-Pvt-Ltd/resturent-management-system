a72082b85e768c9bbc7e6b6fe62b7ad5062ff509
prasanthaL <prasantha.trivextech@gmail.com>
2026-01-30 16:00:45 +0530

    added

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_BASE_URL = "http://resturentsystem.runasp.net/api";
-const API_BASE_URL = "http://192.168.1.9:5071/api";
+const API_BASE_URL = "http://resturentsystem.runasp.net/api";
+//const API_BASE_URL = "http://192.168.1.9:5071/api";
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
