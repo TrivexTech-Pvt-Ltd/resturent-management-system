955661927b591bfca9345f643d2b82f423f857b0
Trivex Tech <ashan.trivextech@gmail.com>
2026-02-01 15:54:01 +0530

    mid

---
+++ backend/RestaurantBackend/Models/Dto/DineinCloseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace RestaurantBackend.Models.Dto;
+
+public class DineinCloseDto
+{
+    public string Id { get; set; }
+    public decimal Total { get; set; }
+    public PaymentMethod PaymentMethod { get; set; }
+}
